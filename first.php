ede63567c1870b0f19d706a995d6eab0583e5044
Ritik Bhushan Yadav <ritik.yadav_cs19@gla.ac.in>
2022-02-23 09:19:22 +0530

    First Basic code interface

---
+++ first.php
@@ -0,0 +1,12 @@
+<?php
+//comments in php - single line comment
+# this is also a single line comment
+/*
+    this is multiline comment you can also use commnets to leave out parts of a code line
+*/
+$res = 5 /* + 10 */ + 5;
+echo $res;
+echo "Hello";
+$msg = "First PhP Program";
+echo $msg;
+?>
